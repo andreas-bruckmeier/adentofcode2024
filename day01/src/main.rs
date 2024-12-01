0f9006c4773ec35d3e145af87fef0a657228b0b9
Andreas Bruckmeier <andreas.bruckmeier@neteleven.de>
2024-12-01 13:23:12 +0100

    day1, part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,30 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::iter::zip;
 
+fn part1(list1: &Vec<u32>, list2: &Vec<u32>) -> u32 {
+    zip(list1, list2).map(|i| i.0.abs_diff(*i.1)).sum()
+}
+
+fn part2(list1: &Vec<u32>, list2: &Vec<u32>) -> u32 {
+    let mut m: HashMap<u32, u32> = HashMap::new();
+    for x in list2 {
+        *m.entry(*x).or_default() += 1;
+    }
+    let mut result: u32 = 0;
+    for number in list1 {
+        let increment = number.saturating_mul(m.get(number).cloned().unwrap_or_default());
+        result = result.saturating_add(increment);
+    }
+    result
+}
+
 fn main() {
-    let lines = BufReader::new(File::open("input.txt").unwrap())
+    let lines: Vec<_> = BufReader::new(File::open("input.txt").unwrap())
         .lines()
-        .map(|l| l.unwrap());
+        .map(|l| l.unwrap())
+        .collect();
 
     let mut list1: Vec<u32> = Vec::new();
     let mut list2: Vec<u32> = Vec::new();
@@ -22,9 +41,9 @@ fn main() {
     list1.sort();
     list2.sort();
 
-    let iter = zip(list1, list2);
-
-    let result: u32 = iter.map(|i| i.0.abs_diff(i.1)).sum();
-
-    println!("{:#?}", result);
+    println!(
+        "part1: {}, part2: {}",
+        part1(&list1, &list2),
+        part2(&list1, &list2)
+    );
 }
