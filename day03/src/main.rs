f75d232c948b26d2227d1f4ace9f591eafcd6ec9
Andreas Bruckmeier <114733974+andreas-bruckmeier@users.noreply.github.com>
2024-12-03 07:04:02 +0100

    Clenaup day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -18,46 +18,7 @@ enum State {
     InDontConditionalB1,
 }
 
-fn part1(input: &str) -> u64 {
-    let mut state = State::SearchingInstruction;
-    let mut temp_operand = String::with_capacity(3);
-    let mut operand1: u64 = 0;
-    let mut result: u64 = 0;
-
-    for char in input.chars() {
-        match state {
-            State::SearchingInstruction if char == 'm' => state = State::M,
-            State::M if char == 'u' => state = State::U,
-            State::U if char == 'l' => state = State::L,
-            State::L if char == '(' => state = State::StartOperand1,
-            State::StartOperand1 if char.is_ascii_digit() => {
-                state = State::Operand1;
-                temp_operand = String::with_capacity(3);
-                temp_operand.push(char);
-            }
-            State::Operand1 if char.is_ascii_digit() => temp_operand.push(char),
-            State::Operand1 if char == ',' => {
-                state = State::DoneOperand1;
-                operand1 = temp_operand.parse().unwrap();
-            }
-            State::DoneOperand1 if char.is_ascii_digit() => {
-                state = State::Operand2;
-                temp_operand = String::with_capacity(3);
-                temp_operand.push(char);
-            }
-            State::Operand2 if char.is_ascii_digit() => temp_operand.push(char),
-            State::Operand2 if char == ')' => {
-                state = State::SearchingInstruction;
-                result += operand1.saturating_mul(temp_operand.parse().unwrap());
-            }
-            _ => state = State::SearchingInstruction,
-        }
-    }
-
-    result
-}
-
-fn part2(input: &str) -> u64 {
+fn part(input: &str, honor_conditionals: bool) -> u64 {
     let mut state = State::SearchingInstruction;
     let mut mul_enabled = true;
     let mut temp_operand = String::with_capacity(3);
@@ -102,7 +63,7 @@ fn part2(input: &str) -> u64 {
             State::Operand2 if char.is_ascii_digit() => temp_operand.push(char),
             State::Operand2 if char == ')' => {
                 state = State::SearchingInstruction;
-                if mul_enabled {
+                if !honor_conditionals || mul_enabled {
                     result += operand1.saturating_mul(temp_operand.parse().unwrap());
                 }
             }
@@ -123,6 +84,6 @@ fn part2(input: &str) -> u64 {
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    println!("part1: {}", part1(&input));
-    println!("part2: {}", part2(&input));
+    println!("part1: {}", part(&input, false));
+    println!("part2: {}", part(&input, true));
 }
