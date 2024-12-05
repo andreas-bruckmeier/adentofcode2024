23d601d1cf43078c028cb14af6c7de56e08dac01
Andreas Bruckmeier <114733974+andreas-bruckmeier@users.noreply.github.com>
2024-12-06 00:42:01 +0100

    part2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,29 +1,36 @@
+use std::cmp::Ordering;
 use std::fs;
 
-fn update_ok(update: &Vec<&str>, rules: &Vec<(&str, &str)>, count_correctly: bool) -> u32 {
-    for (page_index, page) in update.iter().enumerate() {
-        // rules for left side
-        for rule in rules.iter().filter(|rule| rule.0 == *page) {
-            let left = &update[0..page_index];
-            if left.contains(&rule.1) {
-                return 0;
-            }
-        }
-        // rules for right side
-        for rule in rules.iter().filter(|rule| rule.1 == *page) {
-            let right = &update[page_index..];
-            if right.contains(&rule.0) {
-                return 0;
-            }
-        }
-    }
+enum UpdateStatus {
+    Correct(u32),
+    Incorrect(u32),
+}
 
-    let foo = update.len().saturating_div(2);
-    return update[foo].parse().unwrap();
+fn check_update(update: &Vec<&str>, rules: &[(&str, &str)]) -> UpdateStatus {
+    let mut sorted_update = update.clone();
+    sorted_update.sort_by(|a, b| {
+        if rules.iter().any(|rule| *rule == (*a, *b)) {
+            return Ordering::Less;
+        }
+        if rules.iter().any(|rule| *rule == (*b, *a)) {
+            return Ordering::Greater;
+        }
+        Ordering::Equal
+    });
+
+    if *update == sorted_update {
+        UpdateStatus::Correct(update[update.len().saturating_div(2)].parse().unwrap())
+    } else {
+        UpdateStatus::Incorrect(
+            sorted_update[sorted_update.len().saturating_div(2)]
+                .parse()
+                .unwrap(),
+        )
+    }
 }
 
 fn main() {
-    let input = fs::read_to_string("test.txt").unwrap();
+    let input = fs::read_to_string("input.txt").unwrap();
     let (rules, updates) = input.split_once("\n\n").unwrap();
     let rules: Vec<_> = rules.lines().map(|l| l.split_once("|").unwrap()).collect();
     let updates: Vec<_> = updates
@@ -31,8 +38,20 @@ fn main() {
         .map(|l| l.split(",").collect::<Vec<_>>())
         .collect();
 
-    let result1 = updates.iter().map(|u| update_ok(&u, &rules, true)).sum::<u32>();
-    let result2 = updates.iter().map(|u| update_ok(&u, &rules, false)).sum::<u32>();
+    let result1 = updates
+        .iter()
+        .map(|u| match check_update(u, &rules) {
+            UpdateStatus::Correct(v) => v,
+            UpdateStatus::Incorrect(_) => 0,
+        })
+        .sum::<u32>();
+    let result2 = updates
+        .iter()
+        .map(|u| match check_update(u, &rules) {
+            UpdateStatus::Correct(_) => 0,
+            UpdateStatus::Incorrect(v) => v,
+        })
+        .sum::<u32>();
     println!("part1: {}", result1);
     println!("part2: {}", result2);
 }
